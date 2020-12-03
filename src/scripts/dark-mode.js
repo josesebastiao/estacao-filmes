0787b21596eede0b9c94992fcbdc0c06b0d66f6c
Mariana Mattos <marianasmmattos@gmail.com>
2020-12-03 08:53:20 -0300

    full darkmode

--- src/scripts/dark-mode.js
+++ src/scripts/dark-mode.js
@@ -1,1 +1,10 @@
-console.log('dark-mode')
+const darkBtn = document.querySelector('#btnDark');
+const darkBtn2 = document.querySelector('#btnDark2');
+
+const darkMode = () => {
+    document.body.classList.toggle('darkmode');
+    console.log('dark-mode')
+}
+
+darkBtn.addEventListener('click', darkMode)
+darkBtn2.addEventListener('click', darkMode)
