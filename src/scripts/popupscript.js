c25c55c4b30fb8ae6927e001b7c2f68007cbb8fb
RailtonFagnerAzevedo <railtonfagner@gmail.com>
2020-12-03 20:47:27 -0300

    exibindo pop em index, favorites e about

--- src/scripts/popupscript.js
+++ src/scripts/popupscript.js
@@ -1,11 +1,15 @@
-let imagens = document.querySelectorAll('img');
+let imagens = document.querySelectorAll('.card');
 let janelapop = document.querySelector('.pop');
 let corpo = document.querySelector('body');
 let heart = document.querySelector('.favourite');
 
 for (let imagem of imagens) {
     imagem.onmouseover = function() {
+        
+        console.log(imagem.offsetTop)
+
         janelapop.style.zIndex = 1;
+        janelapop.style.opacity = 1;
         if ((imagem.offsetLeft + imagem.offsetWidth + janelapop.offsetWidth) <= corpo.offsetWidth) {
             janelapop.style.left = `${imagem.offsetLeft + imagem.offsetWidth}px`;
         } else {
@@ -15,19 +19,23 @@ for (let imagem of imagens) {
     };
     
     imagem.onmouseleave = function() {
-        janelapop.style.zIndex = -1;  
+        janelapop.style.zIndex = -1; 
+        janelapop.style.opacity = 0; 
     };
 
     janelapop.onmouseover = function() {
         janelapop.style.zIndex = 1;
+        janelapop.style.opacity = 1;
     };
 
     janelapop.onmouseleave = function() {
         janelapop.style.zIndex = -1;
+        janelapop.style.opacity = 0;
     };
 };
 
-heart.onclick = function() {
+heart.onclick = function(ev) {
+    ev.preventDefault();
     if (heart.style.fill == "rgb(235, 47, 160)") {
         heart.style.fill = "#0E2137";
     } else {
