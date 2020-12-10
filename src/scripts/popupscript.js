b9aa6601233bab926f3d32fc8fdc68d0d11d81cf
RailtonFagnerAzevedo <railtonfagner@gmail.com>
2020-12-10 00:00:33 -0300

    Consumindo api ao clicar subcategoria Desktop

--- src/scripts/popupscript.js
+++ src/scripts/popupscript.js
@@ -15,14 +15,51 @@ async function buscarGeneroFilme(){
 };
 
 buscarGeneroFilme();
-let t = setInterval(() => {
+//clicar categoria
+let subcategories = document.getElementsByClassName("subcategories");
+for(let subcategory of subcategories){
+    subcategory.onclick = function() {
+        const sectionCards = document.getElementsByClassName('cards')[0];
+        while (sectionCards.firstChild) {
+            sectionCards.removeChild(sectionCards.lastChild)
+        };
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=54d5d6e26602b36d146edeb90b272ae5&language=pt-BR&page=2&with_genres="+subcategory.id)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(filmes){
+            filmes.results.forEach(
+                (filme, indice) => {
+                    sectionCards.innerHTML += `
+                    <div class="card" id="${indice}">
+                        <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/${filme.poster_path}" alt="">
+                        <div class="card-footer">
+                        <p>${filme.title}</p>
+                        </div>
+                    </div>
+                    `
+                }
+            );
+            sessionStorage.setItem('filmes',JSON.stringify(filmes.results));
+            preenchePopup();
+        })
+        .catch(console.log.bind(console));
+    }
+};
+
+console.log(generos);
+let t = 500;
+setTimeout(() => {
+    preenchePopup();
+},t);
+
+function preenchePopup() {
     let filmes = JSON.parse(sessionStorage.getItem('filmes'));
     sessionStorage.clear();
     let imagens = document.getElementsByClassName('card');
     let janelapop = document.querySelector('.pop');
     let corpo = document.querySelector('body'); 
     let heart = document.querySelector('.favourite');
-
     for (let imagem of imagens) {
         imagem.onmouseover = function() {
             let generoFilme = "";
@@ -80,7 +117,7 @@ let t = setInterval(() => {
         }; 
     };
     clearInterval(t);
-}, 500);
+};
 
 function buscaDiretores(idFilme){
     fetch(`https://api.themoviedb.org/3/movie/${idFilme}/credits?api_key=54d5d6e26602b36d146edeb90b272ae5&language=pt-BR`)
@@ -103,3 +140,5 @@ function buscaDiretores(idFilme){
     })
     .catch(console.log.bind(console));
 };
+
+buscarPopulares();
