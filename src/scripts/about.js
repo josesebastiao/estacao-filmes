e3a697541eb05b798f54a6ff5f2d0654c835f3fd
RailtonFagnerAzevedo <railtonfagner@gmail.com>
2020-12-15 02:02:11 -0300

    adicionados filmes about.html popup não funcionando

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,27 +1,29 @@
+const filmesfavoritos = [274,228150,807,550,1592];
 let filmes = [];
-let x = 0;
-for (let i=0; i < localStorage.length; i ++){
-    let key = localStorage.key(i);
-    if (key === "dark") {continue}; 
-    let valores = localStorage.getItem(key);
-    filmes.push(JSON.parse(valores));
+
+for (let i = 0; i < filmesfavoritos.length; i ++){
+    let key = filmesfavoritos[i];
     fetch(`https://api.themoviedb.org/3/movie/${key}?api_key=54d5d6e26602b36d146edeb90b272ae5&language=pt-BR`)
     .then(function(response){
         return response.json();
     })
     .then(function(filme){
-        const sectionCards = document.getElementsByClassName('cards')[0];
+        const sectionCards = document.getElementsByClassName('our-cards')[0];
         sectionCards.innerHTML += `
-        <div class="card checked" id="${x}">
+        <div class="card checked" id="${i}">
             <img src="https://image.tmdb.org/t/p/w500/${filme.poster_path}" alt="">
             <div class="card-footer">
             <p>${filme.title}</p>
             </div>
         </div>
         `
-        x ++;
+        console.log(filme);
+        filmes.push(filme);
     })
     .catch(console.log.bind(console));
 };
 
+console.log(filmes);
+
 sessionStorage.setItem('filmes',JSON.stringify(filmes));
+
